9de1851cee34b4cdbcc73951257bfe6b0ee3487d
muchirisworld <waburimuchiri@gmail.com>
2026-01-15 12:08:30 +0300

    refactor: move unused elements into archive

--- src/routes/auth/sign-in/index.tsx
+++ src/routes/auth/sign-in/index.tsx
@@ -1,6 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { SignIn } from '@clerk/tanstack-react-start'
-import { SocialAuthButtons } from '@/components/elements/social-auth-provider'
 
 export const Route = createFileRoute('/auth/sign-in/')({
   component: RouteComponent,
